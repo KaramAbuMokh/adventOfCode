f29fc9887a9012d3f3b037753fa5e5df335fcfb1
KaramAbuMokh <abu.mokh.karam@gmail.com>
2021-09-01 04:46:07 +0300

    finishing floor buzzle

---
+++ floor buzzle/result.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+fs.readFile('input.txt',(error,input)=>{
+    console.time('time')
+
+    error ? console.log('errrrrrr'):null
+    input=input.toString()
+    let cnt=0
+
+    for (let index = 0; index < input.length; index++) {
+        const element = input[index];
+        element === "("? cnt++:cnt--
+    }
+
+    console.log(input.length)
+
+    console.timeEnd('time')
+})
+if(cnt===-1){
+    console.log(index+1)
+    break
+}
