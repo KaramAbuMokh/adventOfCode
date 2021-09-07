061eb379ef8011b807cde23b37fb54e8c8273376
KaramAbuMokh <abu.mokh.karam@gmail.com>
2021-09-07 16:35:38 +0300

    finishing day-2

---
+++ day-2/task-1/result.js
@@ -0,0 +1,24 @@
+const fs=require('fs');
+
+
+fs.readFile('../input.txt',(err,input)=>{
+    let lines=input.toString().split('\n');
+    let count=0
+    lines.forEach(element => {
+
+        let dims=element.split('x')
+        let x1=Number(dims[0])
+        let x2=Number(dims[1])
+        let x3=Number(dims[2])
+
+        
+
+        count=count+(2*x1*x2)+(2*x1*x3)+(2*x2*x3)
+        count=count+Math.min(x1*x2,x1*x3,x2*x3)
+
+        
+    });
+
+    console.log(count)
+})
+
